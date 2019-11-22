1834fb85fa8bde10dd027b853c5f759c0ae827a9
vichanse <vicky.nsenga@gmail.com>
2019-11-22 05:40:49 +0100

    refactor: add library in the description

--- projects/button/src/lib/button.component.ts
+++ projects/button/src/lib/button.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from "@angular/core";
   selector: "lib-button",
   template: `
     <p>
-      My button works :-)!
+      My library button works :-)!
     </p>
   `,
   styles: []
